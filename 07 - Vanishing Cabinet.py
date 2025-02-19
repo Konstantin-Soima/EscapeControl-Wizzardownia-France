5380ddd74ac5b69eaa77448376e5ba1fc433e7f9
Konstantin-Soima <konstantin.soyma@gmail.com>
2025-02-19 10:04:49 +0100

    cabinet code in new style

--- 07 - Vanishing Cabinet.py
+++ 07 - Vanishing Cabinet.py
@@ -1,35 +1,45 @@
 from api import EscapeControlAPI
 from time import sleep
-from time import time
 
 api = EscapeControlAPI()
 
 mainDev = 1
 cabinet1Dev = 5
 cabinet2Dev = 6
-maglock1 = 17
-maglock2 = 18
-doorsPin1 = 8
-doorsPin2 = 8
-#B- dev 5 = UV
-opens = 0
-api.GPIOSet(mainDev, 20, False)  # Chains lock
-api.GPIOSetAnalog(cabinet1Dev,12,0)
+
+maglockCabinetDoor1 = 17  # Controlled by sensor on cabinet2Dev (Door 1)
+maglockCabinetDoor2 = 16  # Controlled by sensor on cabinet1Dev (Door 2)
+
+chainsLockPin = 20
+cabinetDoorSensorCabinetDoor1 = 8   # Sensor for cabinet door 1 on cabinet1Dev
+cabinetDoorSensorCabinetDoor2 = 9   # Sensor for cabinet door 2 on cabinet2Dev
+
+cabinetUVControlPin = 11
+cabinetOpensCounter = 0
+
+api.GPIOSet(mainDev, chainsLockPin, False)  # Initialize chains lock
+api.GPIOSetAnalog(cabinet1Dev, cabinetUVControlPin, 0)
 while True: #TODO: сделать рабочим на протяжении 10 минут, потом выключать
-    if api.GPIORead(cabinet1Dev,doorsPin1):
-        api.GPIOSet(mainDev,maglock2,True)
+    # Control for cabinet door 1 (sensor on cabinet1Dev)
+    if api.GPIORead(cabinet1Dev, cabinetDoorSensorCabinetDoor1):
+        api.GPIOSet(mainDev, maglockCabinetDoor1, True)
     else:
-        api.GPIOSet(mainDev,maglock2,False)
-    if api.GPIORead(cabinet2Dev,doorsPin2):
-        api.GPIOSet(mainDev,maglock1,True)
-        opens += 1
-        if opens == 10:
-            api.LocksUnlock(9)
+        api.GPIOSet(mainDev, maglockCabinetDoor1, False)
+    
+    # Control for cabinet door 2 (sensor on cabinet2Dev)
+    if api.GPIORead(cabinet2Dev, cabinetDoorSensorCabinetDoor2):
+        api.GPIOSet(mainDev, maglockCabinetDoor2, True)
+        cabinetOpensCounter += 1
     else:
-        api.GPIOSet(mainDev,maglock1,False)
-    if not api.GPIORead(cabinet2Dev,doorsPin2) and not api.GPIORead(cabinet1Dev,doorsPin1):
-        api.GPIOSetAnalog(cabinet1Dev,12,255)
+        api.GPIOSet(mainDev, maglockCabinetDoor2, False)
+
+    if cabinetOpensCounter == 10 or cabinetOpensCounter == 15: #somebody in 2nd room
+        api.LocksUnlock(9)
+
+    # If both cabinet door sensors are active (doors closed), turn on the UV LED; otherwise, turn it off.
+    if not api.GPIORead(cabinet1Dev, cabinetDoorSensorCabinetDoor1) and not api.GPIORead(cabinet2Dev, cabinetDoorSensorCabinetDoor2):
+        api.GPIOSetAnalog(cabinet1Dev, cabinetUVControlPin, 255)
     else:
-        api.GPIOSetAnalog(cabinet1Dev,12,0)
+        api.GPIOSetAnalog(cabinet1Dev, cabinetUVControlPin, 0)
+        
     sleep(0.2)
-    #TODO: считать что больше двух вышло
